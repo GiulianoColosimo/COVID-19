57879bef8c8205272a7b44bc8483519de38e7713
Giuliano Colosimo <gln.colosimo@gmail.com>
2020-03-26 23:25:46 +0100

    add comments

--- covid19_gc_regional_data.R
+++ covid19_gc_regional_data.R
@@ -1,17 +1,31 @@
+# load packages
+x <- c("dplyr")
+lapply(x, require, character.only = T)
+rm(x)
+
+# change wd
 setwd("dati-regioni/")
 
+# load path to csv files
 files <- list.files(path = ".",
                     pattern = "*.csv",
                     full.names = T)
 
+# remove redundant files
 files <- files[-c((length(files)-1):length(files))]
 
+# load only file names
 files_names <- list.files(path = ".",
                           pattern = "*.csv",
-                          full.names = F) # only file names
+                          full.names = F) 
 
+# remove redundant files
 files_names <- files_names[-c((length(files_names)-1):length(files_names))]
 
+# load the actual data in a list
 myfiles  <-  lapply(files,
                     read.csv,
-                    header = TRUE)                   
+                    header = TRUE)
+
+# change names in file list
+
